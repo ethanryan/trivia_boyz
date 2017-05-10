db75c86d611fa1df2dddc5575e553fb474cdbc43
ethanryan <ethanryan@gmail.com>
2017-05-10 14:37:33 -0400

    filling grid with random answers

--- trivia_boyz_client/src/js/index.js
+++ trivia_boyz_client/src/js/index.js
@@ -1,6 +1,27 @@
 $(document).ready(function() {
   console.log("What's up, Doc?")
 
+
+  function shuffle(array) {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+      // And swap it with the current element.
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+    return array;
+  }
+
+  // Used like so
+  // var arr = [2, 11, 37, 42];
+  // arr = shuffle(arr);
+  // console.log(arr);
+
   //var x
 
 //is this returning a promise or an object?
@@ -13,26 +34,32 @@ $(document).ready(function() {
       // $("#1 .cell1").html(data.a[0].answer)
 
       var arrayOfAnswers = data.a
+      var rowID = 1
+      var trigger = [9, 19, 29, 39, 49, 59, 69, 79, 89, 99]
+
       console.log(data.a);
 
-      arrayOfAnswers.forEach(function(answerObj, i){
-        var rowOne = $(`#1 .cell${i}`).html(arrayOfAnswers[i].answer)
-        var rowTwo = $(`#2 .cell${i}`).html(arrayOfAnswers[i].answer)
-        return rowOne + rowTwo
+      shuffledArray = shuffle(arrayOfAnswers)
+
+      shuffledArray.forEach(function(answerObj, i){
+        // the first index
+        // every tenth element, to go to the next row
+        console.log(answerObj.answer);
+        if (i === 0) {
+          $(`#${rowID} .cell${i+1}`).html(shuffledArray[i].answer) //first answer
+        } else if ( trigger.includes(i) ) { //if index ends in 9...
+          $(`#${rowID} .cell${10}`).html(shuffledArray[i].answer) //fill in 10th cell
+          rowID++ //and go to the next row
+        } else {
+          $(`#${rowID} .cell${(i + 1) % 10}`).html(shuffledArray[i].answer) //fill in every other cell
+        }
       })
-
-
-      // arrayOfAnswers.forEach(function(answerObj, i){
-      //   //---return $(`#1 .cell${i}`).html(array[i].answer)
-      //   answerObj.forEach(function(answer, i){
-      //     return $(`#${i} .cell${i}`).html(array[i].answer)
-      //   })
-      // })
     }, error: function(error) {
       console.log(error)
     }
   })
 })
+
 
 
 //$("#1 .cell1").text("words on the page")

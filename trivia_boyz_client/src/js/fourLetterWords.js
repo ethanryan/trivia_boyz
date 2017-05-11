738b51f42c0b9345cb888c60cc670500b878e04e
christopher-hague <christopher.a.hague@gmail.com>
2017-05-11 14:26:25 -0400

    upated gameplay, separation of concerns

--- trivia_boyz_client/src/js/fourLetterWords.js
+++ trivia_boyz_client/src/js/fourLetterWords.js
@@ -1,88 +1,81 @@
-$(function() {
-  $(".answer").click(function() {
-    if(this.innerHTML.length === 4 && count > 0) {
-      $(this).css("background", "green")
-      console.log($(this).css("background", "green"));
-      score += 2
-      $("#score").html(`Score: ${renderScore()}`)
-    } else if(count > 0 && $(this).css("background", "white")) {
-      $(this).css("background", "red")
-      score -= 4
-      $("#score").html(`Score: ${renderScore()}`)
+$("#gameButton").click(function() {
+  $(function() {
+
+    $(".answer").click(function() {
+      if(this.innerHTML.length === 4 && count > 0 && $(this).css("background-color") === "rgb(255, 255, 255)") {
+        $(this).css("background-color", "green")
+        score += 2
+        $("#score").html(`Score: ${renderScore()}`)
+      } else if(count > 0 && $(this).css("background-color") === "rgb(255, 255, 255)") {
+        debugger
+        $(this).css("background", "red")
+        score -= 4
+        $("#score").html(`Score: ${renderScore()}`)
+      }
+    })
+  })
+
+  function shuffle(array) {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+      // And swap it with the current element.
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
     }
+    return array;
+  }
+
+  $.ajax({
+    dataType: 'json',
+    method: "GET",
+    url: "http://localhost:3000",
+    success: function(data) {
+      $("#question").text(data.q.question)
+      // $("#1 .cell1").html(data.a[0].answer)
+
+      var arrayOfAnswers = data.a
+      var rowID = 101
+      var trigger = [9, 19, 29, 39, 49, 59, 69, 79, 89, 99]
+
+      var shuffledAnswers = shuffle(arrayOfAnswers)
+      var randomIndices = []
+      for(let i = 1; i <= 100; i++) {
+        randomIndices.push(i)
+      }
+      shuffle(randomIndices)
+      var firstFortyAnswers = shuffledAnswers.splice(0, 40)
+      var firstFortyIndices = randomIndices.splice(0, 40)
+
+      console.log(shuffledAnswers.length)
+
+
+      firstFortyAnswers.forEach(function(answerObj, i){
+        $(`#${firstFortyIndices[i]}`).text(firstFortyAnswers[i].answer)
+      })
+
+      var interval = setInterval(addAnswer, 200)
+
+      function addAnswer() {
+        if(shuffledAnswers.length) {
+          var curAnswer = shuffledAnswers.splice(0, 1)
+          var curIndex = randomIndices.splice(0, 1)
+          if ($(`#${curIndex}`).css("background-color") !== "rgb(255, 0, 0)") {
+            $(`#${curIndex}`).text(curAnswer[0].answer)
+          }
+        } else {
+          clearInterval(interval)
+        }
+      }
+    }, error: function(error) {
+      console.log(error)
+    }
+
+
+
   })
 })
-
-function shuffle(array) {
-  var currentIndex = array.length, temporaryValue, randomIndex;
-  // While there remain elements to shuffle...
-  while (0 !== currentIndex) {
-    // Pick a remaining element...
-    randomIndex = Math.floor(Math.random() * currentIndex);
-    currentIndex -= 1;
-    // And swap it with the current element.
-    temporaryValue = array[currentIndex];
-    array[currentIndex] = array[randomIndex];
-    array[randomIndex] = temporaryValue;
-  }
-  return array;
-}
-
-$.ajax({
-  dataType: 'json',
-  method: "GET",
-  url: "http://localhost:3000",
-  success: function(data) {
-    $("#question").text(data.q.question)
-    // $("#1 .cell1").html(data.a[0].answer)
-
-    var arrayOfAnswers = data.a
-    var rowID = 101
-    var trigger = [9, 19, 29, 39, 49, 59, 69, 79, 89, 99]
-
-    var shuffledAnswers = shuffle(arrayOfAnswers)
-    var randomIndices = []
-    for(let i = 1; i <= 100; i++) {
-      randomIndices.push(i)
-    }
-    shuffle(randomIndices)
-    var firstFortyAnswers = shuffledAnswers.splice(0, 40)
-    var firstFortyIndices = randomIndices.splice(0, 40)
-
-    console.log(randomIndices)
-    console.log(firstFortyIndices)
-    console.log(shuffledAnswers)
-    console.log(firstFortyAnswers)
-
-
-    firstFortyAnswers.forEach(function(answerObj, i){
-      $(`#${firstFortyIndices[i]}`).text(shuffledAnswers[i].answer)
-    })
-
-    setInterval(function() {
-      debugger
-      // destructively gran first elem of answers
-      var curAnswer = shuffledAnswers.splice(0, 1)
-      // destructively grab first elem of shuffled indices
-      var curIndex = randomIndices.splice(0, 1)
-      // jquery to the cell with id of that first index
-      // put the answer as the text of that cell
-      $(`#${curIndex}`).text(curAnswer[0].answer)
-    }, 200)
-  }, error: function(error) {
-    console.log(error)
-  }
-
-
-
-})
-
-
-// if (i === 0) {
-//   $(`#${rowID} .cell${i+1}`).text(shuffledAnswers[i].answer) //first answer
-// } else if ( trigger.includes(i) ) { //if index ends in 9...
-//   $(`#${rowID} .cell${10}`).text(shuffledArray[i].answer) //fill in 10th cell
-//   rowID++ //and go to the next row
-// } else {
-//   $(`#${rowID} .cell${(i + 1) % 10}`).text(shuffledArray[i].answer) //fill in every other cell
-// }
--- trivia_boyz_client/src/js/index.js
+++ trivia_boyz_client/src/js/index.js
@@ -1,7 +1,7 @@
-$(document).ready(function() {
-  console.log("What's up, Doc?")
-  })
-
+// $(document).ready(function() {
+//   console.log("What's up, Doc?")
+//   })
+$("#gameButton").click(function() {
   var count = 15;
   var counter = setInterval(timer, 1000);
 
@@ -9,6 +9,7 @@ $(document).ready(function() {
     count = count-1;
     if (count <= 0) {
       clearInterval(counter);
+      alert("Game Over")
     }
     $("#timer").html(`Timer: ${count}`);
   }
@@ -18,62 +19,4 @@ $(document).ready(function() {
   function renderScore() {
     return score;
   }
-
-  // $(function() {
-  //   $(".answer").click(function() {
-  //     if(this.innerHTML.length === 4 && count > 0) {
-  //       $(this).css("background", "green")
-  //       console.log($(this).css("background", "green"));
-  //       score += 2
-  //       $("#score").html(`Score: ${renderScore()}`)
-  //     } else if(count > 0 && $(this).css("background", "white")) {
-  //       $(this).css("background", "red")
-  //       score -= 4
-  //       $("#score").html(`Score: ${renderScore()}`)
-  //     }
-  //   })
-  // })
-
-  // function shuffle(array) {
-  //   var currentIndex = array.length, temporaryValue, randomIndex;
-  //   // While there remain elements to shuffle...
-  //   while (0 !== currentIndex) {
-  //     // Pick a remaining element...
-  //     randomIndex = Math.floor(Math.random() * currentIndex);
-  //     currentIndex -= 1;
-  //     // And swap it with the current element.
-  //     temporaryValue = array[currentIndex];
-  //     array[currentIndex] = array[randomIndex];
-  //     array[randomIndex] = temporaryValue;
-  //   }
-  //   return array;
-  // }
-
-  // $.ajax({
-  //   dataType: 'json',
-  //   method: "GET",
-  //   url: "http://localhost:3000",
-  //   success: function(data) {
-  //     $("#question").text(data.q.question)
-  //     // $("#1 .cell1").html(data.a[0].answer)
-  //
-  //     var arrayOfAnswers = data.a
-  //     var rowID = 1
-  //     var trigger = [9, 19, 29, 39, 49, 59, 69, 79, 89, 99]
-  //
-  //     shuffledArray = shuffle(arrayOfAnswers)
-  //
-  //     shuffledArray.forEach(function(answerObj, i){
-  //       if (i === 0) {
-  //         $(`#${rowID} .cell${i+1}`).text(shuffledArray[i].answer) //first answer
-  //       } else if ( trigger.includes(i) ) { //if index ends in 9...
-  //         $(`#${rowID} .cell${10}`).text(shuffledArray[i].answer) //fill in 10th cell
-  //         rowID++ //and go to the next row
-  //       } else {
-  //         $(`#${rowID} .cell${(i + 1) % 10}`).text(shuffledArray[i].answer) //fill in every other cell
-  //       }
-  //     })
-  //   }, error: function(error) {
-  //     console.log(error)
-  //   }
-  // })
+}

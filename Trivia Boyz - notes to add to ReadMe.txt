01040d944dabd2fc369fabe8c44e30f0d99b77cd
ethanryan <ethanryan@gmail.com>
2017-08-06 18:43:41 -0400

    moved API calls to src /js/index file and added const baseUrl variable

--- Trivia Boyz - notes to add to ReadMe.txt
+++ Trivia Boyz - notes to add to ReadMe.txt
@@ -1,6 +1,6 @@
 Trivia Boyz - notes to add to ReadMe:
 
-For this app, need to install toasty. 
+For this app, need to install toasty.
 
 Directions here:
 
@@ -9,3 +9,20 @@ http://rubentd.com/toasty/
 then run bundle install
 
 also run rake:db reset
+
+
+
+An app is a piece of software that can run
+through a web browser or even offline on any electronic device.
+
+Apps may or may not have a connection to the internet.
+App is a modern take on the word software.
+
+You typically only hear it in reference to a mobile app
+or a small piece of software that's running on a website.
+
+It's used to describe anything that isn't a full-fledged software program.
+There are three main types of apps: desktop, mobile, and web.
+
+Example: Google Maps APIs lets developers embed Google Maps
+on webpages using a JavaScript or Flash interface.
--- trivia_boyz_client/src/js/index.js
+++ trivia_boyz_client/src/js/index.js
@@ -1,6 +1,39 @@
+// $(document).ready(function() {
+//   $("body").toasty();
+// })
+
+
+const baseUrl = 'http://localhost:3000'
+// const baseUrl = 'https://word-nerds-api.herokuapp.com'
+
 $(document).ready(function() {
+  console.log("What's up, Doc?")
+
   $("body").toasty();
-})
+
+  var secondData = ""
+  $.ajax({
+    method: "GET",
+    url: `${baseUrl}/games/highscore`,
+    success: function(data) {
+      console.log(data)
+      $("#highScore").text(data.join(" "))
+    }
+  }).then(function() {
+
+    $.ajax({
+      method: "GET",
+      url: `${baseUrl}/games/questions`,
+      success: function(data) {
+        console.log(`Questions: ${data}`);
+        data.forEach(function(question, i) {
+          secondData = secondData + `<option id=${i}>` + question + "</option>"
+        })
+      }
+    })
+  });
+});
+
 
 
 var count

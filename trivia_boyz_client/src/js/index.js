782027bfae444acdbc85b075db734e68b42d1195
ethanryan <ethanryan@gmail.com>
2017-08-07 19:39:40 -0400

    changed baseUrl to Heroku

--- trivia_boyz_client/src/js/index.js
+++ trivia_boyz_client/src/js/index.js
@@ -3,8 +3,10 @@
 // })
 
 
-const baseUrl = 'http://localhost:3000'
+// const baseUrl = 'http://localhost:3000'
 // const baseUrl = 'https://word-nerds-api.herokuapp.com'
+const baseUrl = 'https://trivia-boyz.herokuapp.com'
+// const baseUrl = 'https://trivia_boyz_api_production.herokuapp.com'
 
 $(document).ready(function() {
   console.log("What's up, Doc?")
